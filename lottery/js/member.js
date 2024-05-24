dbd5dc6aff8f895e6aea5977bef278fc6a2acc92
lz <iwasd@qq.com>
2024-05-24 16:06:55 +0800

    点名

---
+++ lottery/js/member.js
@@ -0,0 +1,152 @@
+var member = [
+  {
+    "name": "No.01"
+  },
+  {
+    "name": "No.02"
+  },
+  {
+    "name": "No.03"
+  },
+  {
+    "name": "No.04"
+  },
+  {
+    "name": "No.05"
+  },
+  {
+    "name": "No.06"
+  },
+  {
+    "name": "No.07"
+  },
+  {
+    "name": "No.08"
+  },
+  {
+    "name": "No.09"
+  },
+  {
+    "name": "No.10"
+  },
+  {
+    "name": "No.11"
+  },
+  {
+    "name": "No.12"
+  },
+  {
+    "name": "No.13"
+  },
+  {
+    "name": "No.14"
+  },
+  {
+    "name": "No.15"
+  },
+  {
+    "name": "No.16"
+  },
+  {
+    "name": "No.17"
+  },
+  {
+    "name": "No.18"
+  },
+  {
+    "name": "No.19"
+  },
+  {
+    "name": "No.20"
+  },
+  {
+    "name": "No.21"
+  },
+  {
+    "name": "No.22"
+  },
+  {
+    "name": "No.23"
+  },
+  {
+    "name": "No.24"
+  },
+  {
+    "name": "No.25"
+  },
+  {
+    "name": "No.26"
+  },
+  {
+    "name": "No.27"
+  },
+  {
+    "name": "No.28"
+  },
+  {
+    "name": "No.29"
+  },
+  {
+    "name": "No.30"
+  },
+  {
+    "name": "No.31"
+  },
+  {
+    "name": "No.32"
+  },
+  {
+    "name": "No.33"
+  },
+  {
+    "name": "No.34"
+  },
+  {
+    "name": "No.35"
+  },
+  {
+    "name": "No.36"
+  },
+  {
+    "name": "No.37"
+  },
+  {
+    "name": "No.38"
+  },
+  {
+    "name": "No.39"
+  },
+  {
+    "name": "No.40"
+  },
+  {
+    "name": "No.41"
+  },
+  {
+    "name": "No.42"
+  },
+  {
+    "name": "No.43"
+  },
+  {
+    "name": "No.44"
+  },
+  {
+    "name": "No.45"
+  },
+  {
+    "name": "No.46"
+  },
+  {
+    "name": "No.47"
+  },
+  {
+    "name": "No.48"
+  },
+  {
+    "name": "No.49"
+  },
+  {
+    "name": "No.50"
+  },
+]
